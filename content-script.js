83ab363c04018bde8cf2fbb494275dca4d30503d
Samane <baran.s2s.26.75@gmail.com>
2022-02-16 02:16:35 +0330

    📦 NEW: add no accepted answer status

--- content-script.js
+++ content-script.js
@@ -1,3 +1,9 @@
-document
-	.querySelector(".accepted-answer")
-	.scrollIntoView({ behavior: "smooth" });
+const acceptedAnswer = document.querySelector(".accepted-answer");
+const header = document.getElementsByTagName("header")[0];
+
+if (acceptedAnswer) {
+	header.style.backgroundColor = "#d8f3dc";
+	acceptedAnswer.scrollIntoView({ behavior: "smooth" });
+} else {
+	header.style.backgroundColor = "#fbf8cc";
+}
