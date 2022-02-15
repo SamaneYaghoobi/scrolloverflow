4759240e29c5652c380e8601e38985b261369941
Samane <baran.s2s.26.75@gmail.com>
2022-02-16 01:43:16 +0330

    🐛 FIX: Add onUpdated listener

--- background.js
+++ background.js
@@ -1,15 +1,18 @@
-async function getCurrentTab() {
-	let queryOptions = { active: true, currentWindow: true };
-	let [tab] = await chrome.tabs.query(queryOptions);
-	return tab;
-}
-
-chrome.tabs.onActivated.addListener(async () => {
-	let tab = await getCurrentTab();
+const executeScript = (tab) => {
 	if (tab.url.includes("stackoverflow")) {
 		chrome.scripting.executeScript({
 			target: { tabId: tab.id },
 			files: ["content-script.js"],
 		});
+	}
+};
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+	chrome.tabs.get(activeInfo.tabId, (tab) => executeScript(tab));
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, _, tab) {
+	if (tab.url) {
+		chrome.tabs.get(tabId, executeScript(tab));
 	}
 });
