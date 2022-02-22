d8535530ad8dd923c33250773dc050b1c71f2279
Samane <baran.s2s.26.75@gmail.com>
2022-02-22 17:46:06 +0330

    📦 NEW: add dark theme

--- content-script.js
+++ content-script.js
@@ -1,11 +1,23 @@
 (function () {
 	const acceptedAnswer = document.querySelector(".accepted-answer");
 	const header = document.getElementsByTagName("header")[0];
+	const body = document.getElementsByTagName("body")[0];
+	const currentTheme = body.classList.contains("theme-dark") ? "dark" : "light";
+	const backgroundColor = {
+		dark: {
+			success: "#3c5d4e",
+			fail: "#5d573c",
+		},
+		light: {
+			success: "#d8f3dc",
+			fail: "#fdf7e2",
+		},
+	};
 
 	if (acceptedAnswer) {
-		header.style.backgroundColor = "#d8f3dc";
+		header.style.backgroundColor = backgroundColor[currentTheme].success;
 		acceptedAnswer.scrollIntoView({ behavior: "smooth" });
 	} else {
-		header.style.backgroundColor = "#fdf7e2";
+		header.style.backgroundColor = backgroundColor[currentTheme].fail;
 	}
 })();
